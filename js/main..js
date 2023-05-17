4870aae8a58f7318029f4b7ab24a51cbb49e9797
iteenfk <125781734+iteenfk@users.noreply.github.com>
2023-05-17 20:30:31 +0900

    Add files via upload

---
+++ js/main..js
@@ -0,0 +1,24 @@
+"use strict"
+
+function S() {
+   let li = document.createElement("li");
+   let inp = document.getElementById("inp").value;
+   li.textContent = inp;
+
+   let out = document.getElementById("out");
+   out.appendChild(li);
+
+   document.getElementById("inp").value = "";
+
+}
+
+function C(){
+    const ul = document.getElementById("out");
+    while (ul.firstChild != null) {
+        ul.removeChild(ul.firstChild);
+    }
+}
+
+function P(){
+    window.print()
+}
