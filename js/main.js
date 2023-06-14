dfbc8caeba64b2b05c4922212fa65d720afc0387
iteenfk <125781734+iteenfk@users.noreply.github.com>
2023-06-14 20:41:04 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,41 +1,94 @@
-"use strict"
+    "use strict"
+    
+    // 収入と支出を管理するための変数
+    let incList = [];
+    let expList = [];
 
-let words = [];
-function A(){
-    const in1 = document.getElementById("in1")
-    const in2 = document.getElementById("in2")
-    const trim1 = in1.value.trim();
-    const trim2 = in2.value.trim();
-
-    if (!trim1 || !trim2) {
-        alert("単語と意味を入力してください。");
+    // 収入の追加
+    function Ainc() {
+      const inc = parseFloat(document.getElementById("inc").value);
+      if (!inc) {
+        alert("収入が入力されていません。");
         return;
+      }
+      incList.push(inc);
+      Update();
+      UpincList();
+      document.getElementById("inc").value = ""
     }
-    words.push({trim1, trim2});
-    D();
-    in1.value = "";
-    in2.value = "";
-} 
 
-function D() {
-  const out = document.getElementById("out");
-  out.innerHTML = "";
-  words.forEach((x) =>{
-    const li = document.createElement("li");
-    li.textContent = `${x.trim1}: ${x.trim2}`;
-    out.appendChild(li);
-    console.log(li);
-    });
-}
-
-function C(){
-    const ul = document.getElementById("out");
-    while(ul.firstChild != null) {
-        ul.removeChild(ul.firstChild);
+    // 支出の追加
+    function Aexp() {
+      const exp = parseFloat(document.getElementById("exp").value);
+      if (!exp) {
+        alert("支出が入力されていません。");
+        return;
+      }
+      expList.push(exp);
+      Update();
+      UpexpList();
+      document.getElementById("exp").value = ""
     }
-    let word = [];
-}
 
-function P(){
-    window.print()
-}
+    // 残高の計算と表示更新
+    function Update(){
+
+      let totalinc = 0;
+      incList.forEach(x =>{
+      totalinc += x;
+      });
+ 
+      let totalexp = 0;
+      expList.forEach(x =>{
+      totalexp += x;
+      });
+ 
+       const balance = totalinc - totalexp;
+       document.getElementById("zan").textContent = balance.toLocaleString();
+    }
+
+    // 収入リストの表示更新
+    function UpincList() {
+      const list = document.getElementById("inc-list");
+      list.innerHTML = "";
+      for (let i = 0; i < incList.length; i++) {
+        const item = document.createElement("li");
+        item.textContent = incList[i].toLocaleString();
+        list.appendChild(item);
+      }
+    }
+
+    
+    // 支出リストの表示更新
+    function UpexpList() {
+        const list = document.getElementById("exp-list");
+        list.innerHTML = "";
+        for (let i = 0; i < expList.length; i++) {
+            const item = document.createElement("li");
+            item.textContent = expList[i].toLocaleString();
+            list.appendChild(item);
+        }
+    }
+
+    function P(){
+      window.print();
+    }
+
+    function C(){
+      const ulinc = document.getElementById("inc-list");
+      while (ulinc.firstChild != null) {
+          ulinc.removeChild(ulinc.firstChild);
+      }
+
+      const ulexp = document.getElementById("exp-list");
+      while (ulexp.firstChild != null){
+          ulexp.removeChild(ulexp.firstChild);
+      }
+
+      document.getElementById("zan").textContent = 0;
+
+      incList.splice(0);
+      expList.splice(0);
+
+    }
+    
